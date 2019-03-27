3f74d508f17c2d0c7c4fba842e2a5ca0e2c7f44a
vishnu umakanthan <vvichugit@gmail.com>
2019-03-27 15:20:01 -0500

    initila commit

---
+++ indi.js
@@ -0,0 +1,13 @@
+function navbar(){
+
+  
+  
+  if(document.getElementById("link_label").style.display == "block"){
+  	document.getElementById("link_label").style.display = "none";
+  }else{
+  	document.getElementById("link_label").style.display = "block";
+  };
+
+
+
+}
